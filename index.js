6d69e83bbd26df813986507e125e65a307266a26
olapina <100097501+olapina@users.noreply.github.com>
2022-11-03 13:05:10 -0500

    Updating token type

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function run() {
       payload: { repository },
     } = github.context;
 
-    const octokit = new github.GitHub(githubToken);
+    const octokit = new github.getOctokit(githubToken);
     const { data: targetBranches } = await octokit.git.listMatchingRefs({
       owner: repository.owner.login,
       repo: repository.name,

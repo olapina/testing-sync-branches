5686b1995c80dfc8b92c5f299d5dad3b417b00ec
olapina <olga.yu.lapina@gmail.com>
2022-11-03 14:59:01 -0500

    Updating API path

--- create-branch.js
+++ create-branch.js
@@ -1,12 +1,12 @@
 async function createBranch(octokit, context, branch) {
   try {
-    await octokit.repos.getBranch({
+    await octokit.rest.repos.getBranch({
       ...context.repo,
       branch,
     });
   } catch (error) {
     if (error.name === "HttpError" && error.status === 404) {
-      await octokit.git.createRef({
+      await octokit.rest.git.createRef({
         ref: `refs/heads/${branch}`,
         sha: context.sha,
         ...context.repo,
